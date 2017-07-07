b5ffd4747e7175f4736f5d3dfb591ead0b2429fa
Samantha Smith <ssmith1975.soc@gmail.com>
2017-07-06 20:46:45 -0500

    Added screenshots; removed unused using statements in HelloWorld.aspx.cs.

--- SimpleWeb/HelloWorld.aspx.cs
+++ SimpleWeb/HelloWorld.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using SimplyWriterLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace SimpleWeb {
     public partial class HelloWorld : System.Web.UI.Page {
